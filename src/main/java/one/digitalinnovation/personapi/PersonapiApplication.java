00949772e2125cb9799a8a3c914cb8d5109ebde1
caaarllosr <carlosrvmelo@gmail.com>
2021-05-12 20:09:16 -0300

    add ComponentScan

--- src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
+++ src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
@@ -1,9 +1,12 @@
 package one.digitalinnovation.personapi;
 
+import one.digitalinnovation.personapi.controller.PersonController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses= PersonController.class)
 public class PersonapiApplication {
 
 	public static void main(String[] args) {
