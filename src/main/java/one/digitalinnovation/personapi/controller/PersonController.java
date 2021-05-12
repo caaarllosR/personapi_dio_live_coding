f3f7e775c64529d23dbc7667cdd7c9b15ad40bc4
caaarllosr <carlosrvmelo@gmail.com>
2021-05-12 17:07:55 -0300

    GetMapping function getBook in PersonController class

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -4,12 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
+
+    @GetMapping
     public String getBook() {
-        return "API TEST!";
+        return "API Test!";
     }
 }
